7e508c7c8150082ecf4d2ee2f309a0a3645b0fc3
sss <soe@ss.ss>
2016-06-25 18:03:04 -0400

    fix bug

--- find_salt.pl
+++ find_salt.pl
@@ -1,3 +1,29 @@
+# Copyright (c) 2016-.. #John
+#
+# Author: #John <pocolab.com@gmail.com>
+# URL: http://www.pocolab.com
+# Version: 1.0.0
+
+# Commentary:
+
+# Decode illuminati cipher
+
+# License:
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
 use strict;
 use Digest::MD5 qw(md5_hex); 
 sub valid_i{
@@ -24,7 +50,7 @@ sub func{
 		if(valid_i($i)){
 			$c=chr($i);
 			$str=$p.$s.$c;
-			$md5=md5_hex($md5);
+			$md5=md5_hex($str);
 			print("    s: $s; str: $str; md5: $md5\n");
 			if($h eq $md5){
 				goto ex;
